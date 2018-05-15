f0bd8c04b73051f8f26282d12fdd6a87e1d50157
Eric Pendergrass <ependergrass@suse.com>
2018-05-15 16:43:13 -0600

    Translate device names to standard names

--- collect.py
+++ collect.py
@@ -18,6 +18,9 @@ PARSER.add_argument('-u', '--url', metavar='url',
 ARGS = PARSER.parse_args()
 
 IP = ARGS.ip
+if IP is None and url is None:
+    raise ValueError("Must provide one of --ip or --url options")
+
 if IP:
     observer_url = "http://" + IP + "/livedata.htm"
 
@@ -33,23 +36,42 @@ values = tree.xpath('//input[@name]/@value')
 if (len(names) != len(values)):
     raise IndexError("Unexpected html structure")
 
+# Device to standard name translation
+name_table = {'windir':  'winddir',
+              'avgwind':  'windspeedmph',
+              'outHumi':  'humidity',
+              'AbsPress':  'baromin',
+              'rainofhourly':  'hourlyrainin',
+              'rainofdaily':  'rainin',
+              'rainofweekly':  'weeklyrainin',
+              'rainofmonthly':  'monthlyrainin',
+              'rainofyearly':  'yearlyrainin',
+              'inHumi':  'indoorhumidity',
+              'avgwind':  'windspeedmph',
+              'gustspeed':  'windgustmph',
+              'inTemp':  'indoortempf',
+              'outTemp':  'tempf',
+              'CurrTime':  'device_time',
+              }
+
 pairs = {}
 weather = {'weather': pairs}
 weather['time'] = time.time()
 weather['url'] = observer_url
 
 for name, value in zip(names, values):
+    # Use fuzzy matching on names in case they change
     if re.search('temp', name, re.I):
-        pairs[name] = value
+        pairs[name_table[name]] = value
 
     if re.search('curr', name, re.I) and re.search('time', name, re.I):
-        pairs['unit_time'] = value
+        pairs[name_table[name]] = value
 
     if re.search('pres', name, re.I):
         pairs[name] = value
 
     if re.search('humi', name, re.I):
-        pairs[name] = value
+        pairs[name_table[name]] = value
 
     if re.search('uv', name, re.I):
         pairs[name] = value
@@ -58,10 +80,16 @@ for name, value in zip(names, values):
         pairs[name] = value
 
     if re.search('rain', name, re.I) and re.search('of', name, re.I):
-        pairs[name] = value
+        pairs[name_table[name]] = value
 
     if re.search('gust', name, re.I) or re.search('wind', name, re.I):
-        pairs[name] = value
+        if name in name_table:
+            pairs[name_table[name]] = value
+        else:
+            pairs[name] = value
 
+for name, value in pairs.iteritems():
+    if re.search(r'--', value):
+        pairs[name] = None
 
 print json.dumps(weather)
